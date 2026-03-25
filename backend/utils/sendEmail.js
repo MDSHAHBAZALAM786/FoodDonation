7cdbe489b64bb3a2ae1cf20725f8c93bb14dc5cc
Prince9558 <singhania9558@gmail.com>
2026-03-26 01:18:28 +0530

    Fix email IPv6 issue

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,12 +7,15 @@ const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      }
     });
 
     const mailOptions = {
@@ -24,9 +27,10 @@ const sendEmail = async (options) => {
 
     await transporter.sendMail(mailOptions);
     console.log("✅ Email sent successfully");
+
   } catch (error) {
     console.error("❌ Email error:", error);
-    throw error; // VERY IMPORTANT
+    throw error;
   }
 };
 
